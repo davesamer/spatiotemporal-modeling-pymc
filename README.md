98182c8e11ba3a43f638f7836ea37bdd2e967775
David <david.roebl@yahool.com>
2023-12-06 16:52:54 +0100

    create models and train in jupyter notebook

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Using the probabilistic programming framework pymc to create spatiotemporal models for diverse topics
 
+Task 1: Create predictive bayesian models for californian house price prediction
+
+- Methodology: Bayesian Linear Regression.
+
+  - Full pooling (coefficients are same for all counties)
+  - No pooling (coefficients vary from county to county. Coefficients independently estimated for each county)
+  - Partial pooling (coefficients vary from county to county. Each county intercept is informed by state-leve intercept)
+  - Gaussian process priors (coefficients vary from county to county. Coefficients of counties near to each other are assumed to be more similar, than counties far apart)
+
 # Install requirements
 
 ```
@@ -15,3 +24,21 @@ Download the shp-file of the Californian countries from
 https://catalog.data.gov/dataset/tiger-line-shapefile-2019-state-california-current-county-subdivision-state-based
 
 The californian housing dataset can be loaded from sklearn.datasets
+
+## TODOs
+
+- Californian Housing data
+
+  - Implement Gaussian Priors
+  - Train models on decreasing data sizes. Evaluate and compare all models
+  - Can we restructure code (generalize)
+  - Evaluate & compare models (BIC, AIC)
+  - Create nice plots & maps
+
+- Hierachical BNN
+
+## Overall notes:
+
+- What if for hierachical models the train data does not contain all spatial groups?
+  - What if the test data contains group that are not in the train data?
+  - I think I have to specify all spatial groups either way for the model. Does not matter if the groups exist in the training data or not
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pymc as pm
+import numpy as np
 
 """
 models
@@ -19,7 +20,7 @@ def create_blr_full_pooling(X: pd.DataFrame,
                             y: pd.Series) -> pm.Model:
     """Normal Bayesian Linear Regression Model"""
 
-    with pm.Model() as blr_full_pooling:
+    with pm.Model() as model:
 
         # create data containers for all the features
         medinc = pm.MutableData('medinc', X.MedInc)
@@ -32,14 +33,14 @@ def create_blr_full_pooling(X: pd.DataFrame,
             'median_house_value', y)
 
         # specify priors for the features
-        intercept = pm.Normal('intercept', 0, 10)
-        beta_medinc = pm.Normal('beta_medinc', 0, 10)
-        beta_house_age = pm.Normal('beta_house_age', 0, 10)
-        beta_ave_rooms = pm.Normal('beta_ave_rooms', 0, 10)
-        beta_ave_bedrms = pm.Normal('beta_ave_bedrms', 0, 10)
-        beta_population = pm.Normal('beta_population', 0, 10)
-        beta_ave_occup = pm.Normal('beta_ave_occup', 0, 10)
-        sigma = pm.HalfCauchy("sigma", beta=10)
+        intercept = pm.Normal('intercept', 0, 1)
+        beta_medinc = pm.Normal('beta_medinc', 0, 1)
+        beta_house_age = pm.Normal('beta_house_age', 0, 1)
+        beta_ave_rooms = pm.Normal('beta_ave_rooms', 0, 1)
+        beta_ave_bedrms = pm.Normal('beta_ave_bedrms', 0, 1)
+        beta_population = pm.Normal('beta_population', 0, 1)
+        beta_ave_occup = pm.Normal('beta_ave_occup', 0, 1)
+        sigma = pm.HalfCauchy("sigma", beta=1)
 
         # Logistic Regression
         mean = intercept + \
@@ -53,7 +54,7 @@ def create_blr_full_pooling(X: pd.DataFrame,
         likelihood = pm.Normal("y", mu=mean, sigma=sigma,
                                observed=median_house_value)
 
-        return blr_full_pooling
+        return model
 
 
 def create_blr_no_pooling(X: pd.DataFrame,
@@ -62,7 +63,7 @@ def create_blr_no_pooling(X: pd.DataFrame,
                           spatial_grouping_var: str) -> pm.Model:
     """Hierachical Bayesian Linear Regression Model with no pooling"""
 
-    with pm.Model(coords=coords) as blr_full_pooling:
+    with pm.Model(coords=coords) as model:
 
         # create data containers for all the features
         medinc = pm.MutableData('medinc', X.MedInc)
@@ -77,30 +78,134 @@ def create_blr_no_pooling(X: pd.DataFrame,
             'spatial_group_idx', X[spatial_grouping_var])
 
         # specify priors for the features
-        intercept = pm.Normal('intercept', 0, 10, dims=("spatial_groups"))
-        beta_medinc = pm.Normal('beta_medinc', 0, 10, dims=("spatial_groups"))
-        beta_house_age = pm.Normal(
-            'beta_house_age', 0, 10, dims=("spatial_groups"))
-        beta_ave_rooms = pm.Normal(
-            'beta_ave_rooms', 0, 10, dims=("spatial_groups"))
-        beta_ave_bedrms = pm.Normal(
-            'beta_ave_bedrms', 0, 10, dims=("spatial_groups"))
-        beta_population = pm.Normal(
-            'beta_population', 0, 10, dims=("spatial_groups"))
-        beta_ave_occup = pm.Normal(
-            'beta_ave_occup', 0, 10, dims=("spatial_groups"))
-        sigma = pm.HalfCauchy("sigma", beta=10)
+        intercept = pm.Normal('intercept', 0, 1, dims=("spatial_groups"))
+        beta_medinc = pm.Normal('beta_medinc', 0, 1)
+        beta_house_age = pm.Normal('beta_house_age', 0, 1)
+        beta_ave_rooms = pm.Normal('beta_ave_rooms', 0, 1)
+        beta_ave_bedrms = pm.Normal('beta_ave_bedrms', 0, 1)
+        beta_population = pm.Normal('beta_population', 0, 1)
+        beta_ave_occup = pm.Normal('beta_ave_occup', 0, 1)
+        sigma = pm.HalfCauchy("sigma", beta=1)
 
         # Logistic Regression
         mean = intercept[spatial_group_idx] + \
-            beta_medinc[spatial_group_idx] * medinc + \
-            beta_house_age[spatial_group_idx] * house_age + \
-            beta_ave_rooms[spatial_group_idx] * ave_rooms + \
-            beta_ave_bedrms[spatial_group_idx] * ave_bedrms + \
-            beta_population[spatial_group_idx] * population + \
-            beta_ave_occup[spatial_group_idx] * ave_occup
+            beta_medinc * medinc + \
+            beta_house_age * house_age + \
+            beta_ave_rooms * ave_rooms + \
+            beta_ave_bedrms * ave_bedrms + \
+            beta_population * population + \
+            beta_ave_occup * ave_occup
 
         likelihood = pm.Normal("y", mu=mean, sigma=sigma,
                                observed=median_house_value)
 
-        return blr_full_pooling
+        return model
+
+
+def create_blr_partial_pooling(X: pd.DataFrame,
+                               y: pd.Series,
+                               coords: dict,
+                               spatial_grouping_var: str) -> pm.Model:
+    """Hierachical Bayesian Linear Regression Model with no pooling"""
+
+    with pm.Model(coords=coords) as model:
+
+        # create data containers for all the features
+        medinc = pm.MutableData('medinc', X.MedInc)
+        house_age = pm.MutableData('house_age', X.HouseAge)
+        ave_rooms = pm.MutableData('ave_rooms', X.AveRooms)
+        ave_bedrms = pm.MutableData('ave_bedrms', X.AveBedrms)
+        population = pm.MutableData('population', X.Population)
+        ave_occup = pm.MutableData('ave_occup', X.AveOccup)
+        median_house_value = pm.MutableData(
+            'median_house_value', y)
+        spatial_group_idx = pm.MutableData(
+            'spatial_group_idx', X[spatial_grouping_var])
+
+        # specify priors for the features
+        intercept_mu = pm.Normal('intercept_mu', 0, 1)
+        intercept_sigma = pm.HalfCauchy('intercept_sigma', beta=1)
+        intercept = pm.Normal('intercept', intercept_mu,
+                              intercept_sigma, dims=("spatial_groups"))
+
+        beta_medinc = pm.Normal('beta_medinc', 0, 1)
+        beta_house_age = pm.Normal('beta_house_age', 0, 1)
+        beta_ave_rooms = pm.Normal('beta_ave_rooms', 0, 1)
+        beta_ave_bedrms = pm.Normal('beta_ave_bedrms', 0, 1)
+        beta_population = pm.Normal('beta_population', 0, 1)
+        beta_ave_occup = pm.Normal('beta_ave_occup', 0, 1)
+        sigma = pm.HalfCauchy("sigma", beta=1)
+
+        # Logistic Regression
+        mean = intercept[spatial_group_idx] + \
+            beta_medinc * medinc + \
+            beta_house_age * house_age + \
+            beta_ave_rooms * ave_rooms + \
+            beta_ave_bedrms * ave_bedrms + \
+            beta_population * population + \
+            beta_ave_occup * ave_occup
+
+        likelihood = pm.Normal("y", mu=mean, sigma=sigma,
+                               observed=median_house_value)
+
+        return model
+
+
+def create_blr_partial_pooling_gp(X: pd.DataFrame,
+                                  y: pd.Series,
+                                  coords: dict,
+                                  spatial_grouping_var: str,
+                                  spatial_groups_coordinates: np.array) -> pm.Model:
+
+    with pm.Model(coords=coords) as model:
+
+        # create data containers for all the features
+        medinc = pm.MutableData('medinc', X.MedInc)
+        house_age = pm.MutableData('house_age', X.HouseAge)
+        ave_rooms = pm.MutableData('ave_rooms', X.AveRooms)
+        ave_bedrms = pm.MutableData('ave_bedrms', X.AveBedrms)
+        population = pm.MutableData('population', X.Population)
+        ave_occup = pm.MutableData('ave_occup', X.AveOccup)
+        median_house_value = pm.MutableData(
+            'median_house_value', y)
+        spatial_group_idx = pm.MutableData(
+            'spatial_group_idx', X[spatial_grouping_var])
+        spatial_groups_coords = pm.MutableData(
+            "spatial_groups_coords", spatial_groups_coordinates)
+
+        # Create features priors;
+        # They are composed of state level means and the variation of the spatial groups represented by the gp priors
+        intercept_mu = pm.Normal("intercept_mu", mu=0.0, sigma=1.0)
+        intercept_sigma = pm.Exponential("intecept_sigma", 1)
+
+        ls = pm.Gamma(name=f"intercept_ls", alpha=0, beta=1)
+        amplitude = pm.Gamma(name=f"intercept_amplitude", alpha=0, beta=1)
+        cov = amplitude ** 2 * pm.gp.cov.ExpQuad(input_dim=2, ls=ls)
+        latent = pm.gp.Latent(cov_func=cov)
+        gp = latent.prior(
+            f"intercept_gp", X=spatial_groups_coords, jitter=1e-7)
+
+        intercept = pm.Normal('intercept', intercept_mu,
+                              intercept_sigma * gp, dims=("spatial_groups"))
+
+        beta_medinc = pm.Normal('beta_medinc', 0, 1)
+        beta_house_age = pm.Normal('beta_house_age', 0, 1)
+        beta_ave_rooms = pm.Normal('beta_ave_rooms', 0, 1)
+        beta_ave_bedrms = pm.Normal('beta_ave_bedrms', 0, 1)
+        beta_population = pm.Normal('beta_population', 0, 1)
+        beta_ave_occup = pm.Normal('beta_ave_occup', 0, 1)
+        sigma = pm.HalfCauchy("sigma", beta=1)
+
+        # Logistic Regression
+        mean = intercept[spatial_group_idx] + \
+            beta_medinc * medinc + \
+            beta_house_age * house_age + \
+            beta_ave_rooms * ave_rooms + \
+            beta_ave_bedrms * ave_bedrms + \
+            beta_population * population + \
+            beta_ave_occup * ave_occup
+
+        likelihood = pm.Normal("y", mu=mean, sigma=sigma,
+                               observed=median_house_value)
+
+    return model
